58aedf437b227189d85ee356788784c0036a2287
rezendematheus <matheusreisrezende.2002@gmail.com>
2023-04-23 23:42:37 -0300

    fix: tests minor bugs

--- tests/factories/tickets-factory.ts
+++ tests/factories/tickets-factory.ts
@@ -3,12 +3,29 @@ import { TicketStatus, TicketType } from '@prisma/client';
 import { prisma } from '@/config';
 
 export async function createTicketType(params?: Partial<TicketType>) {
+  let isRemote = true;
+  let includesHotel = true;
+  if(params?.isRemote !== undefined && !params.isRemote){
+    isRemote = false;
+  } else if (params?.isRemote){
+    isRemote = true;
+  } else {
+    isRemote = faker.datatype.boolean();
+  }
+  if(params?.includesHotel !== undefined && !params.includesHotel){
+    includesHotel = false;
+  } else if (params?.includesHotel){
+    includesHotel = true;
+  } else {
+    includesHotel = faker.datatype.boolean();
+  }
+
   return prisma.ticketType.create({
     data: {
       name: faker.name.findName(),
       price: faker.datatype.number(),
-      isRemote: params?.isRemote,
-      includesHotel: params?.includesHotel,
+      isRemote: isRemote,
+      includesHotel: includesHotel,
     },
   });
 }
